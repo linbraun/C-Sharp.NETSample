774a5c5ae676b5898fcd8a23435702f633e91355
Lindsey Braun <116319647+linbraun@users.noreply.github.com>
2023-10-12 13:46:02 -0400

    Add files via upload

---
+++ AddMultiplyFourInts/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AddMultiplyFourInts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int int1 = 13;
+            int int2 = 55;
+            int int3 = 123;
+            int int4 = 325;
+
+            int sum = int1 + int2 + int3 + int4;
+            int product = int1 * int2 * int3 * int4;
+
+            Console.WriteLine("The sum of " + int1 + ", " + int2 + ", " + int3 + ", and " + int4 + " = " + sum);
+            Console.WriteLine("The product of " + int1 + ", " + int2 + ", " + int3 + ", and " + int4 + " = " + product);
+        }
+    }
+}
---
+++ ConsoleConvert/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringNumber = "";
+            int intNumber = 0;
+            double doubleNumber = 0;
+
+            Console.WriteLine("Please enter a number: ");
+            stringNumber = Convert.ToString(Console.ReadLine());
+            doubleNumber = Convert.ToDouble(stringNumber);
+            intNumber = Convert.ToInt32(stringNumber);
+
+            Console.WriteLine("This number as a string: " + stringNumber);
+            Console.WriteLine("This number as a double: " + doubleNumber);
+            Console.WriteLine("This number as an int: " + intNumber);
+        }
+    }
+}
